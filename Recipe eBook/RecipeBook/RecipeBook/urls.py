2d164ba19cda4dd9e8c2e8418118aef10b349afc
ngarde2882 <14495+ngarde2882@users.noreply.github.tamu.edu>
2023-06-27 17:22:00 -0500

    Finish Skeleton and start Recipe Model

--- Recipe eBook/RecipeBook/RecipeBook/urls.py
+++ Recipe eBook/RecipeBook/RecipeBook/urls.py
@@ -16,7 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the catalog application
+urlpatterns += [
+    path('recipebookapp/', include('recipebookapp.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+urlpatterns += [
+    path('', RedirectView.as_view(url='recipebook/', permanent=True)),
+]
+
+# Use static() to add URL mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Recipe eBook/RecipeBook/recipebookapp/models.py
+++ Recipe eBook/RecipeBook/recipebookapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class RecipeIngredientModel(models.Model): # TODO: Maybe split this into 2 different ingredient models, one for a standard list of ingredients sitewide holding information about the ingredient and one for a user inventory that has fields local to users
+    """A typical class defining a model, derived from the Model class."""
+
+    # Fields
+    Name = models.CharField(max_length=20, help_text='Ingredient Name')
+    Quantity = models.IntegerField(help_text='Ingredient Amount')
+    # DaysBeforeExpire = models.IntegerField(help_text='Days Before Ingredient Expires') TODO
+    # PurchaseDate = models.DateField(help_text='Day Ingredient Was Purchased') TODO
+    # …
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)."""
+        return self.Name
+
+class RecipeAuthor(models.Model):
+    Name = models.CharField(max_length=25, help_text='Author Name')
+    Org = models.CharField(max_length=25, help_text='Organization Name') # can be null
+
+class RecipeModel(models.Model):
+    """A typical class defining a model, derived from the Model class."""
+
+    # Fields
+    Name = models.CharField(max_length=20, help_text='Recipe Name')
+    # use recipeObj.ingredients.create(Name=,Quantity=) to generate ingredients or recipeObj.ingredients.add(ingredientObj) to add existing ones
+    Ingredients = models.ManyToManyField(RecipeIngredientModel, help_text='Recipe Ingredients')
+    Directions = models.TextField(help_text='Recipe Directions')
+    img = models.ImageField(upload_to='images/', help_text='Recipe Image')
+    mealType = models.CharField(max_length=20, help_text='Type of Meal: entree, snack, bread, drink') # entree, snack, bread, drink
+    # …
+
+    # Metadata
+    class Meta:
+        ordering = ['Name']
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.Name
