89069f6fa821596f4fc12c118d724bc5b75c3c5d
ngarde2882 <ngarde2882@tamu.edu>
2023-06-28 17:20:23 -0700

    updating models, create superuser

--- Recipe eBook/RecipeBook/recipebookapp/admin.py
+++ Recipe eBook/RecipeBook/recipebookapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import RecipeIngredientModel, RecipeAuthor, RecipeModel
 # Register your models here.
+admin.site.register(RecipeIngredientModel)
+admin.site.register(RecipeAuthor)
+admin.site.register(RecipeModel)
